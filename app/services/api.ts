e7e33e472f838bb101af8f9c9a92852415b00c20
“pavankumarpalaka” <sivagovindharao@gmail.com>
2025-09-21 00:16:54 +0530

    small changes

--- app/services/api.ts
+++ app/services/api.ts
@@ -1,7 +1,7 @@
 // services/api.ts
 import * as SecureStore from "expo-secure-store";
 
-const API_URL = "http://192.168.0.183:5000"; // replace with your backend host
+const API_URL = "http://192.168.1.185:5000"; // replace with your backend host
 
 // --- Token helpers ---
 async function getToken() {
